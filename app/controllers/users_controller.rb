41e1fbc027fbab0857c9036ca34d77a5592ae3d8
lehiep071191 <good.boy.0991@gmail.com>
2020-08-22 21:55:35 +0700

    signup success

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+	before_action :find_id, only: :show
+  
+	def show
+	end
+	def new
+	  	@user = User.new
+	 end
+
+  	def create
+	  	@user = User.new(user_params)
+	  	if @user.save
+		  	flash[:success] = "Welcome to the Sample App!"
+		  	redirect_to root_url
+	  	else
+	  		render :new 
+	  	end	
+    end
+
+  	def destroy
+ 	end
+
+  	private 
+  	def find_id
+  		@user = User.find_by(id:params[:id])
+  		if @user.nil?
+  			flash[:danger] = "tài khoản không khả dụng"
+  		end	
+  	end	
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation, :ngaysinh, :diachi, :gioitinh)
+  	end	
+end
